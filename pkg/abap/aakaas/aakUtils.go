02aa47a8080508be4839b9a04b2bfc20d9408256
Rosemarie Bentele <rosemarie.bentele@sap.com>
2021-12-08 15:30:57 +0100

    add to utils file

--- pkg/abap/aakaas/aakUtils.go
+++ pkg/abap/aakaas/aakUtils.go
@@ -6,6 +6,8 @@ import (
 	abapbuild "github.com/SAP/jenkins-library/pkg/abap/build"
 	"github.com/SAP/jenkins-library/pkg/abaputils"
 	"github.com/SAP/jenkins-library/pkg/command"
+	piperhttp "github.com/SAP/jenkins-library/pkg/http"
+	"github.com/SAP/jenkins-library/pkg/log"
 )
 
 type AakUtils interface {
@@ -14,4 +16,44 @@ type AakUtils interface {
 	ReadAddonDescriptor(FileName string) (abaputils.AddonDescriptor, error)
 	GetMaxRuntime() time.Duration
 	GetPollingInterval() time.Duration
+}
+
+type AakBundle struct {
+	*command.Command
+	*piperhttp.Client
+	maxRuntime      time.Duration
+	pollingInterval time.Duration
+}
+
+func (bundle *AakBundle) GetMaxRuntime() time.Duration {
+	return bundle.maxRuntime
+}
+func (bundle *AakBundle) GetPollingInterval() time.Duration {
+	return bundle.pollingInterval
+}
+func (bundle *AakBundle) ReadAddonDescriptor(FileName string) (abaputils.AddonDescriptor, error) {
+	return abaputils.ReadAddonDescriptor(FileName)
+}
+
+func NewAakBundleWithTime(maxRuntime time.Duration, pollingInterval time.Duration) AakUtils {
+	utils := AakBundle{
+		Command:         &command.Command{},
+		Client:          &piperhttp.Client{},
+		maxRuntime:      maxRuntime * time.Minute,
+		pollingInterval: pollingInterval * time.Second,
+	}
+	// Reroute command output to logging framework
+	utils.Stdout(log.Writer())
+	utils.Stderr(log.Writer())
+	return &utils
+}
+func NewAakBundle() AakUtils {
+	utils := AakBundle{
+		Command: &command.Command{},
+		Client:  &piperhttp.Client{},
+	}
+	// Reroute command output to logging framework
+	utils.Stdout(log.Writer())
+	utils.Stderr(log.Writer())
+	return &utils
 }
